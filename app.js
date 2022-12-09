848458b56901c447da0ddfb821b1877ed5fb79cf
parthkk <parthkotecha2@gmail.com>
2022-12-08 20:12:46 -0500

    deployment in progress

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://parth:parthmongodb@cluster0.rbfga7y.mongodb.net/mern?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rbfga7y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
--- util/location.js
+++ util/location.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const HttpError = require("../models/http-error");
 
-const API_KEY = "AIzaSyBBkFQMFwp57P6I0a9MNDDDrdAl79lVoPk";
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordForAddress(address) {
   const response = await axios.get(
